34f52106b8f4088ce7267459999678f3fdf5f535
comet <angry-dev@naver.com>
2025-12-05 13:14:47 +0900

    constant 파일 생성

---
+++ lib/constant/app_constants.dart
@@ -0,0 +1,8 @@
+/// 앱에서 사용하는 상수들을 한 곳에서 관리합니다.
+library;
+
+class AppConstants {
+  static const String adminCode = 'admin';
+  static const String userCode = 'user';
+  static const String loginErrorMsg = '올바른 코드를 입력하세요.';
+}
